775c000ac2c2dc809f9832caab429a0be4808a41
jiggery-pokery <liew.audi+github@gmail.com>
2022-05-20 16:33:21 +0800

    Add VM

---
+++ app/src/main/java/com/example/inventory/viewmodel/InventoryViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.inventory.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItemEntry(
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item = Item(itemName = itemName, itemPrice = itemPrice.toDouble(), quantityInStock = itemCount.toInt())
+
+    fun addNewItem(
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+    fun getItems(): Flow<List<Item>> = itemDao.getItems()
+
+    fun getItem(id: Int): Flow<Item> = itemDao.getItem(id)
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
