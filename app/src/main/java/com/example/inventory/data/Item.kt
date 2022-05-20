fcfcd9bbc45d1b83028322d049cf14b605137a98
jiggery-pokery <liew.audi+github@gmail.com>
2022-05-20 16:33:13 +0800

    Fix item property name

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -8,6 +8,6 @@ import androidx.room.PrimaryKey
 data class Item(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "name") val itemName: String,
-    @ColumnInfo(name = "price") val price: Double,
+    @ColumnInfo(name = "price") val itemPrice: Double,
     @ColumnInfo(name = "quantity") val quantityInStock: Int
 )
