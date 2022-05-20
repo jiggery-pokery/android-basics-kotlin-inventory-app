bfd7d82d43562ee2ee2a60708c5684ca4f56481b
jiggery-pokery <liew.audi+github@gmail.com>
2022-05-20 17:14:46 +0800

    Add ItemAdapter

---
+++ app/src/main/java/com/example/inventory/ItemAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.inventory
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inventory.data.Item
+import com.example.inventory.databinding.ItemListItemBinding
+
+class ItemAdapter(
+    private val onItemClicked: (Item) -> Unit
+) : ListAdapter<Item, ItemAdapter.ItemViewHolder>(DiffCallback) {
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val viewHolder = ItemViewHolder(
+            ItemListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onItemClicked(getItem(position))
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ItemViewHolder(
+        private var binding: ItemListItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SimpleDateFormat")
+        fun bind(item: Item) {
+            binding.itemName.text = item.itemName
+            binding.itemPrice.text = item.itemPrice.toString()
+            binding.itemQuantity.text = item.quantityInStock.toString()
+        }
+    }
+}
